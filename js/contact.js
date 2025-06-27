d60cf1e8974a540f9350481857356667e37bc8ec
M NAVED <mdnadvisory@gmail.com>
2025-06-27 20:50:47 +0530

    js

--- js/contact.js
+++ js/contact.js
@@ -7,14 +7,14 @@
   function done_func(response) {
     message.fadeIn().removeClass('alert-danger').addClass('alert-success');
     message.text("✅ Message sent successfully!");
-    setTimeout(() => message.fadeOut(), 3000);
+    setTimeout(() => message.fadeOut(), 4000);
     form[0].reset();
   }
 
   function fail_func(data) {
     message.fadeIn().removeClass('alert-success').addClass('alert-danger');
     message.text("❌ Failed to send message. Please try again.");
-    setTimeout(() => message.fadeOut(), 3000);
+    setTimeout(() => message.fadeOut(), 4000);
   }
 
   form.submit(function (e) {
